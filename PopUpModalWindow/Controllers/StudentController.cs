5ddc7269a9a03164a1528ab5fc1d5d4dd53ce925
Md Shohag Mia <shohag.mia@datasoft-bd.com>
2021-06-15 17:32:19 +0600

    Add project files.

---
+++ PopUpModalWindow/Controllers/StudentController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PopUpModalWindow.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PopUpModalWindow.Controllers
+{
+    public class StudentController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View(new List<Student>());
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Student model)
+        {
+            return View();
+        }
+    }
+}
---
+++ PopUpModalWindow/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PopUpModalWindow.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
